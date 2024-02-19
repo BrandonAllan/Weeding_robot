accdb1b56334ca68e35b94f9b9d18ea939e21498
Brandon <allanbrandon34@gmail.com>
2024-02-19 01:40:49 -0400

    Created robot_description

---
+++ agri_robot_description/launch/gazebo.launch.py
@@ -0,0 +1,40 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    package_name = 'agri_robot_description'
+
+    display = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','display.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+    
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'agri_robot',
+                                   "-x", '0.0',
+                                   "-y", '0.0',
+                                   "-z", '1.1',
+                                   "-Y", '0.0'],
+                        output='screen')
+    
+    return LaunchDescription([
+        display,    
+        gazebo,
+        spawn_entity,
+    ])
