02948bdc8cf0c52abd7019ce077e09544a47649e
Brandon <allanbrandon34@gmail.com>
2024-02-29 09:37:38 -0400

    Add joystick

---
+++ robot_joystick/launch/joystick.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    joy_params = os.path.join(get_package_share_directory('robot_joystick'),'config','joystick.yaml')
+
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params],
+         )
+    teleop_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joy_params],
+        remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
+     )
+    
+    return LaunchDescription([
+        joy_node,
+        teleop_node
+    ])
